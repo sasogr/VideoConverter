59633046522686f6d8aeb86df2d9582a5e955146
Dejan Stamenov <dejanstamenov93@outlook.com>
2016-05-05 00:07:34 +0200

    Folder structure creation with nio. NOT WORKING PROPERLY

--- VideoConverter/src/main/java/videoconverter/businesslogic/UserFolderStructure.java
+++ VideoConverter/src/main/java/videoconverter/businesslogic/UserFolderStructure.java
@@ -1,6 +1,10 @@
 package videoconverter.businesslogic;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Creates the /home/videoconverter/{username}/upload and /home/videoconverter/{username}/download folders for storing videos.
@@ -15,12 +19,25 @@ public class UserFolderStructure {
 		this.username = _username;
 	}
 	
-	public boolean CreateUserFolders() throws Exception {
+	public boolean CreateUserFolders() {
 		boolean foldersCreated = false;
 		
-		File videoconverterDir = new File("/home/videoconverter/" + username + "/");
-		File videoconverterUpload = new File("/home/videoconverter/" + username + "/upload/");
-		File videoconverterDownload = new File("/home/videoconverter/" + username + "/download/");
+		// Exception: java.nio.file.AccessDeniedException
+		Path path = Paths.get("/home/videoconverter/" + this.username + "/");
+
+		if(!Files.exists(path)) {
+			try {
+				Files.createDirectories(path);
+			}
+			catch(IOException io) {
+				io.printStackTrace();
+			}
+		}
+		
+		// NOT WORKING - NO EXCEPTION THROWN AT ALL
+		/*File videoconverterDir = new File("/home/videoconverter/" + this.username + "/");
+		File videoconverterUpload = new File("/home/videoconverter/" + this.username + "/upload/");
+		File videoconverterDownload = new File("/home/videoconverter/" + this.username + "/download/");
 		
 		if(!videoconverterDir.exists()) {
 			// Directory does not exist, create it.
@@ -40,7 +57,7 @@ public class UserFolderStructure {
 		}
 		else {
 			foldersCreated = true;
-		}
+		}*/
 
 		return foldersCreated;
 	}
