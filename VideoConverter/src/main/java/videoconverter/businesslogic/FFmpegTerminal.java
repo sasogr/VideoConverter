467aa0010ef34a71253ec239a92700824ef7c48d
Dejan Stamenov <dejanstamenov93@outlook.com>
2016-05-26 21:56:52 +0200

    FFMPEG terminal commands can now be executed

--- VideoConverter/src/main/java/videoconverter/businesslogic/FFmpegTerminal.java
+++ VideoConverter/src/main/java/videoconverter/businesslogic/FFmpegTerminal.java
@@ -1,14 +1,19 @@
 package videoconverter.businesslogic;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
 
+import videoconverter.folderstructure.FolderStructureNaming;
+
 public class FFmpegTerminal {
+	private String username;
 	private String terminalCommand;
 	private StringBuilder executionOutput;
 	private String executionOutputAsString;
 	
 	public FFmpegTerminal() {
+		this.username = "";
 		this.terminalCommand = "";
 		this.executionOutput = new StringBuilder();
 		this.executionOutputAsString = "";
@@ -16,8 +21,11 @@ public class FFmpegTerminal {
 	
 	public String ExecuteCommand() {
 		try {
-			ProcessBuilder pb = new ProcessBuilder(this.terminalCommand);
-			Process process = pb.start();
+			UpdateCommandWithPath();
+			
+			//ProcessBuilder pb = new ProcessBuilder(this.terminalCommand);
+			//Process process = pb.start();
+			Process process = Runtime.getRuntime().exec(this.terminalCommand);
 			
 			BufferedReader inputReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 			String line = null;
@@ -47,7 +55,32 @@ public class FFmpegTerminal {
 		return this.executionOutputAsString;
 	}
 	
+	public void UpdateCommandWithPath() {
+		UpdateUserVideo userVideo = new UpdateUserVideo();
+		userVideo.SetUsername(this.username);
+		String uploadedVideoName = userVideo.GetVideoNameUploaded();
+		
+		FolderStructureNaming folderNaming = new FolderStructureNaming();
+		String videoUploadLocation = folderNaming.GetGlobalPath() + this.username + folderNaming.GetPathUpload() + uploadedVideoName;
+		String videoDownloadLocation = folderNaming.GetGlobalPath() + this.username + folderNaming.GetPathDownload() + "converted_" + uploadedVideoName;
+		
+		
+		if(this.terminalCommand.contains("ffprobe")) {
+			this.terminalCommand += videoUploadLocation;
+		}
+		else if(this.terminalCommand.contains("ffmpeg")) {
+			this.terminalCommand = new StringBuilder(this.terminalCommand).insert(7, "-i " + videoUploadLocation + " ").toString();
+			this.terminalCommand += videoDownloadLocation;
+			
+			//System.err.println("FFMPEG TEST:" + this.terminalCommand);
+		}
+	}
+	
 	public void SetTerminalCommand(String _command) {
 		this.terminalCommand = _command;
+	}
+	
+	public void SetUsername(String _username) {
+		this.username = _username;
 	}
 }
